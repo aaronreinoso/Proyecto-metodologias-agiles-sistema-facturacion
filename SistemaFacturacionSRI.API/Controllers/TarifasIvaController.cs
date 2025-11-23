23af6f7c801374fcc0dc98048c8fd629471761b6
Hlagua <hlagua6116@uta.edu.ec>
2025-11-23 13:38:29 -0500

    enpoint de inva

---
+++ SistemaFacturacionSRI.API/Controllers/TarifasIvaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaFacturacionSRI.Infrastructure.Persistence;
+using SistemaFacturacionSRI.Domain.Entities;
+
+namespace SistemaFacturacionSRI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarifasIvaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TarifasIvaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/tarifasiva
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TarifaIva>>> GetTarifas()
+        {
+            var tarifas = await _context.TarifasIva.ToListAsync();
+            return Ok(tarifas);
+        }
+    }
+}
